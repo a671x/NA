de73e09eb2b6b99b292bd5fef5c2bbd603675380
a671x <49905114+a671x@users.noreply.github.com>
2019-04-29 07:46:50 +0900

    Update fw.js

--- fw.js
+++ fw.js
@@ -1,138 +1,108 @@
-    11: [function(_0x19fe35, _0x209756, _0x476afc) {
+    12: [function(_0x372231, _0x2f1f53, _0x34d99d) {
         'use strict';
-        var _0x1fa79d = function() {
-            function _0x19fe35(_0x19fe35, _0x209756) {
-                for (var _0x476afc = 0x0; _0x476afc < _0x209756['length']; _0x476afc++) {
-                    var _0x1fa79d = _0x209756[_0x476afc];
-                    _0x1fa79d['enumerable'] = _0x1fa79d['enumerable'] || !0x1, _0x1fa79d['configurable'] = !0x0, 'value' in _0x1fa79d && (_0x1fa79d['writable'] = !0x0), Object['defineProperty'](_0x19fe35, _0x1fa79d['key'], _0x1fa79d);
+        var _0x40051f = function() {
+            function _0x372231(_0x372231, _0x2f1f53) {
+                for (var _0x34d99d = 0x0; _0x34d99d < _0x2f1f53['length']; _0x34d99d++) {
+                    var _0x40051f = _0x2f1f53[_0x34d99d];
+                    _0x40051f['enumerable'] = _0x40051f['enumerable'] || !0x1, _0x40051f['configurable'] = !0x0, 'value' in _0x40051f && (_0x40051f['writable'] = !0x0), Object['defineProperty'](_0x372231, _0x40051f['key'], _0x40051f);
                 }
             }
-            return function(_0x209756, _0x476afc, _0x1fa79d) {
-                return _0x476afc && _0x19fe35(_0x209756[_0x55c2('0x3c')], _0x476afc), _0x1fa79d && _0x19fe35(_0x209756, _0x1fa79d), _0x209756;
+            return function(_0x2f1f53, _0x34d99d, _0x40051f) {
+                return _0x34d99d && _0x372231(_0x2f1f53['prototype'], _0x34d99d), _0x40051f && _0x372231(_0x2f1f53, _0x40051f), _0x2f1f53;
             };
         }();
-        var _0x3343bb = function() {
-            function _0x19fe35() {
-                ! function(_0x19fe35, _0x209756) {
-                    if (!(_0x19fe35 instanceof _0x209756)) throw new TypeError('Cannot call a class as a function');
-                }(this, _0x19fe35);
+        var _0x12a12b = function() {
+            function _0x372231() {
+                ! function(_0x372231, _0x2f1f53) {
+                    if (!(_0x372231 instanceof _0x2f1f53)) throw new TypeError('Cannot call a class as a function');
+                }(this, _0x372231);
             }
-            return _0x1fa79d(_0x19fe35, null, [{
-                'key': 'getItemByName',
-                'value': function(_0x19fe35) {
-                    var _0x209756 = Object['keys'](this['main']['gameObjects']['items']),
-                        _0x476afc = !0x0,
-                        _0x1fa79d = !0x1,
-                        _0x3343bb = void 0x0;
-                    try {
-                        for (var _0x53b387, _0x5abcce = _0x209756[Symbol['iterator']](); !(_0x476afc = (_0x53b387 = _0x5abcce['next']())['done']); _0x476afc = !0x0) {
-                            var _0xf8ed24 = _0x53b387['value'],
-                                _0x3e59bd = this['main']['gameObjects']['items'][_0xf8ed24];
-                            if (_0xf8ed24 === _0x19fe35) return _0x3e59bd;
-                        }
-                    } catch (_0x589b31) {
-                        _0x1fa79d = !0x0, _0x3343bb = _0x589b31;
-                    } finally {
-                        try {
-                            !_0x476afc && _0x5abcce['return'] && _0x5abcce['return']();
-                        } finally {
-                            if (_0x1fa79d) throw _0x3343bb;
-                        }
-                    }
-                    return null;
+            return _0x40051f(_0x372231, null, [{
+                'key': 'pressKey',
+                'value': function(_0x372231) {
+                    var _0x2f1f53 = this['main']['game']['scope'][this['main']['obfuscate']['input']['main']][this['main']['obfuscate']['input']['input']]['keys'];
+                    _0x2f1f53[_0x372231] || setTimeout(function() {
+                        _0x2f1f53[_0x372231] = !0x0, setTimeout(function() {
+                            delete _0x2f1f53[_0x372231];
+                        }, 0x5a);
+                    }, 0x0);
                 }
             }, {
-                'key': 'getItemByTypes',
-                'value': function(_0x19fe35, _0x209756) {
-                    var _0x476afc = Object['keys'](this['main']['gameObjects']['items']),
-                        _0x1fa79d = !0x0,
-                        _0x3343bb = !0x1,
-                        _0x441b96 = void 0x0;
-                    try {
-                        for (var _0x33bd13, _0x18b673 = _0x476afc[Symbol['iterator']](); !(_0x1fa79d = (_0x33bd13 = _0x18b673['next']())['done']); _0x1fa79d = !0x0) {
-                            var _0x3de3cb = _0x33bd13['value'],
-                                _0x5458d4 = this['main']['gameObjects']['items'][_0x3de3cb],
-                                _0xb7b060 = !0x0,
-                                _0x3cf9aa = !0x1,
-                                _0x20e7e1 = void 0x0;
-                            try {
-                                for (var _0x59c2ff, _0x3d6869 = _0x209756[Symbol['iterator']](); !(_0xb7b060 = (_0x59c2ff = _0x3d6869['next']())['done']); _0xb7b060 = !0x0) {
-                                    var _0x4e3467 = _0x59c2ff['value'];
-                                    if (_0x5458d4['type'] === _0x4e3467 && _0x3de3cb === _0x19fe35) return _0x3de3cb;
-                                }
-                            } catch (_0x4e22a5) {
-                                _0x3cf9aa = !0x0, _0x20e7e1 = _0x4e22a5;
-                            } finally {
-                                try {
-                                    !_0xb7b060 && _0x3d6869['return'] && _0x3d6869['return']();
-                                } finally {
-                                    if (_0x3cf9aa) throw _0x20e7e1;
-                                }
-                            }
-                        }
-                    } catch (_0x19f44d) {
-                        _0x3343bb = !0x0, _0x441b96 = _0x19f44d;
-                    } finally {
-                        try {
-                            !_0x1fa79d && _0x18b673['return'] && _0x18b673['return']();
-                        } finally {
-                            if (_0x3343bb) throw _0x441b96;
-                        }
-                    }
-                    return null;
+                'key': 'rightClickBinded',
+                'value': function() {
+                    for (var _0x372231 = this['main']['game']['scope'][this['main']['obfuscate']['input']['main']]['binds'], _0x2f1f53 = _0x372231['length'], _0x34d99d = 0x0; _0x34d99d < _0x2f1f53; _0x34d99d++)
+                        if (null != _0x372231[_0x34d99d] && 0x2 == _0x372231[_0x34d99d]['type'] && 0x2 == _0x372231[_0x34d99d]['code']) return !0x0;
+                    return !0x1;
                 }
-            }, {
-                'key': 'getItemsByType',
-                'value': function(_0x19fe35) {
-                    var _0x209756 = [],
-                        _0x476afc = Object['keys'](this['main']['gameObjects']['items']),
-                        _0x1fa79d = !0x0,
-                        _0x3343bb = !0x1,
-                        _0x3a15db = void 0x0;
-                    try {
-                        for (var _0x1e05f5, _0x3ef731 = _0x476afc[Symbol['iterator']](); !(_0x1fa79d = (_0x1e05f5 = _0x3ef731['next']())['done']); _0x1fa79d = !0x0) {
-                            var _0x54cff0 = _0x1e05f5['value'];
-                            this['main']['gameObjects']['items'][_0x54cff0][_0x55c2('0xc4')] === _0x19fe35 && _0x209756[_0x55c2('0xa')]({
-                                'name': this['main']['gameObjects']['items'][_0x54cff0]['name'],
-                                'key': _0x54cff0
-                            });
-                        }
-                    } catch (_0x4e03ef) {
-                        _0x3343bb = !0x0, _0x3a15db = _0x4e03ef;
-                    } finally {
-                        try {
-                            !_0x1fa79d && _0x3ef731['return'] && _0x3ef731['return']();
-                        } finally {
-                            if (_0x3343bb) throw _0x3a15db;
-                        }
-                    }
-                    return _0x209756;
-                }
-            }, {
-                'key': 'getExplosionTypesByDecalType',
-                'value': function(_0x19fe35) {
-                    var _0x209756 = [],
-                        _0x476afc = Object['keys'](this['main']['gameObjects']['explosions']),
-                        _0x1fa79d = !0x0,
-                        _0x3343bb = !0x1,
-                        _0x79dee1 = void 0x0;
-                    try {
-                        for (var _0x3229dd, _0x10b493 = _0x476afc[Symbol['iterator']](); !(_0x1fa79d = (_0x3229dd = _0x10b493['next']())['done']); _0x1fa79d = !0x0) {
-                            var _0x35d444 = _0x3229dd['value'];
-                            this['main']['gameObjects']['explosions'][_0x35d444]['decalType'] === _0x19fe35 && _0x209756[_0x55c2('0xa')](this['main']['gameObjects']['explosions'][_0x35d444]['explosionEffectType']);
-                        }
-                    } catch (_0x57870a) {
-                        _0x3343bb = !0x0, _0x79dee1 = _0x57870a;
-                    } finally {
-                        try {
-                            !_0x1fa79d && _0x10b493['return'] && _0x10b493['return']();
-                        } finally {
-                            if (_0x3343bb) throw _0x79dee1;
-                        }
-                    }
-                    return _0x209756;
-                }
-            }]), _0x19fe35;
+            }]), _0x372231;
         }();
-        _0x209756['exports'] = _0x3343bb;
+        _0x2f1f53['exports'] = _0x12a12b;
+    }, {}],
+    13: [function(_0x2d7a88, _0x5a3142, _0x2597da) {
+        'use strict';
+        var _0x51928b = function() {
+            function _0x2d7a88(_0x2d7a88, _0x5a3142) {
+                for (var _0x2597da = 0x0; _0x2597da < _0x5a3142['length']; _0x2597da++) {
+                    var _0x51928b = _0x5a3142[_0x2597da];
+                    _0x51928b['enumerable'] = _0x51928b['enumerable'] || !0x1, _0x51928b['configurable'] = !0x0, 'value' in _0x51928b && (_0x51928b['writable'] = !0x0), Object['defineProperty'](_0x2d7a88, _0x51928b['key'], _0x51928b);
+                }
+            }
+            return function(_0x5a3142, _0x2597da, _0x51928b) {
+                return _0x2597da && _0x2d7a88(_0x5a3142[_0x55c2('0x3c')], _0x2597da), _0x51928b && _0x2d7a88(_0x5a3142, _0x51928b), _0x5a3142;
+            };
+        }();
+        var _0x38ed24 = function() {
+            function _0x2d7a88() {
+                ! function(_0x2d7a88, _0x5a3142) {
+                    if (!(_0x2d7a88 instanceof _0x5a3142)) throw new TypeError('Cannot call a class as a function');
+                }(this, _0x2d7a88);
+            }
+            return _0x51928b(_0x2d7a88, null, [{
+                'key': 'getDistance',
+                'value': function(_0x2d7a88, _0x5a3142) {
+                    var _0x2597da = _0x5a3142['x'] - _0x2d7a88['x'],
+                        _0x51928b = _0x5a3142['y'] - _0x2d7a88['y'];
+                    return Math['sqrt'](_0x2597da * _0x2597da + _0x51928b * _0x51928b);
+                }
+            }, {
+                'key': 'calculateDistance',
+                'value': function(_0x2d7a88, _0x5a3142, _0x2597da, _0x51928b) {
+                    return Math['sqrt'](Math['pow'](_0x2d7a88 - _0x2597da, 0x2) + Math['pow'](_0x5a3142 - _0x51928b, 0x2));
+                }
+            }, {
+                'key': 'calculateXCoordLineIntersection',
+                'value': function(_0x2d7a88, _0x5a3142, _0x2597da, _0x51928b) {
+                    return (_0x51928b * _0x2d7a88 - _0x2597da * _0x5a3142) / _0x51928b;
+                }
+            }, {
+                'key': 'calculateYCoordLineIntersection',
+                'value': function(_0x2d7a88, _0x5a3142, _0x2597da, _0x51928b) {
+                    return (_0x51928b * _0x2d7a88 - _0x2597da * _0x5a3142) / -_0x2597da;
+                }
+            }, {
+                'key': 'isWindow',
+                'value': function(_0x2d7a88) {
+                    return void 0x0 !== _0x2d7a88[_0x55c2('0xa2')] && _0x2d7a88[_0x55c2('0xa2')][_0x3e93('0xa8')](_0x55c2('0x27')) > -0x1;
+                }
+            }, {
+                'key': 'getMouseScreenPos',
+                'value': function() {
+                    return this['main']['game']['scope'][this['main']['obfuscate']['input']['main']][this['main']['obfuscate']['input']['input']]['mousePos'];
+                }
+            }, {
+                'key': 'getMousePointPos',
+                'value': function() {
+                    return this['main']['game']['scope'][this['main']['obfuscate']['camera']]['screenToPoint'](this['getMouseScreenPos']());
+                }
+            }, {
+                'key': 'getMapPosFromWorldPos',
+                'value': function(_0x2d7a88, _0x5a3142, _0x2597da) {
+                    return {
+                        'x': _0x2d7a88['mapSprite']['x'] - _0x2d7a88['mapSprite']['width'] / 0x2 + _0x2597da['x'] / _0x5a3142['width'] * _0x2d7a88['mapSprite']['width'],
+                        'y': _0x2d7a88['mapSprite']['y'] + _0x2d7a88['mapSprite'][_0x55c2('0x8d')] / 0x2 - _0x2597da['y'] / _0x5a3142[_0x55c2('0x8d')] * _0x2d7a88['mapSprite']['height']
+                    };
+                }
+            }]), _0x2d7a88;
+        }();
+        _0x5a3142['exports'] = _0x38ed24;
     }, {}],
