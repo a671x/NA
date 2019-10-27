fedee902b3967871c66c4f7c1f9405cfd5e223b4
a671x <49905114+a671x@users.noreply.github.com>
2019-10-27 13:30:27 +0900

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,10 +1,10 @@
-    "753d6e4b": function(e, t, a) {
+    "753d6e4b": function (e, t, a) {
         "use strict";
 
         function r(e) {
             o(void 0 !== e), this.creator = {
                 type: e
-            }, this.Be = [], this.activeCount = 0
+            }, this.mt = [], this.activeCount = 0
         }
 
         function i(e) {
@@ -13,31 +13,31 @@
         var o = a("0e566746"),
             s = a("f398b7c7");
         r.prototype = {
-            alloc: function() {
-                for (var e = null, t = 0; t < this.Be.length; t++)
-                    if (!this.Be[t].active) {
-                        e = this.Be[t];
+            alloc: function () {
+                for (var e = null, t = 0; t < this.mt.length; t++)
+                    if (!this.mt[t].active) {
+                        e = this.mt[t];
                         break
                     }
-                return e || (e = new this.creator.type, this.Be.push(e)), e.active = !0, e.o(), this.activeCount++, e
+                return e || (e = new this.creator.type, this.mt.push(e)), e.active = !0, e.o(), this.activeCount++, e
             },
-            free: function(e) {
-                if (e.n(), e.active = !1, this.activeCount--, this.Be.length > 128 && this.activeCount < this.Be.length / 2) {
-                    for (var t = [], a = 0; a < this.Be.length; a++) this.Be[a].active && t.push(this.Be[a]);
-                    this.Be = t
+            free: function (e) {
+                if (e.n(), e.active = !1, this.activeCount--, this.mt.length > 128 && this.activeCount < this.mt.length / 2) {
+                    for (var t = [], a = 0; a < this.mt.length; a++) this.mt[a].active && t.push(this.mt[a]);
+                    this.mt = t
                 }
             },
-            p: function() {
-                return this.Be
+            p: function () {
+                return this.mt
             }
         }, i.prototype = {
-            registerType: function(e, t) {
+            registerType: function (e, t) {
                 this.types[e] = t
             },
-            getObjById: function(e) {
+            getObjById: function (e) {
                 return this.idToObj[e]
             },
-            getTypeById: function(e, t) {
+            getTypeById: function (e, t) {
                 var a = this.getObjById(e);
                 if (!a) {
                     var r = {
@@ -50,16 +50,16 @@
                 }
                 return a.__type
             },
-            updateObjFull: function(e, t, a, r) {
+            updateObjFull: function (e, t, a, r) {
                 var i = this.getObjById(t),
                     o = !1;
                 return void 0 === i && (i = this.types[e].alloc(), i.__id = t, i.__type = e, this.idToObj[t] = i, this.seenCount++, o = !0), i.c(a, !0, o, r), i
             },
-            updateObjPart: function(e, t, a) {
+            updateObjPart: function (e, t, a) {
                 var r = this.getObjById(e);
                 r ? r.c(t, !1, !1, a) : (console.log("updateObjPart, missing object", e), s.storeGeneric("objectPoolErr", "updateObjPart"))
             },
-            deleteObj: function(e) {
+            deleteObj: function (e) {
                 var t = this.getObjById(e);
                 void 0 === t ? (console.log("deleteObj, missing object", e), s.storeGeneric("objectPoolErr", "deleteObj")) : (this.types[t.__type].free(t), delete this.idToObj[e])
             }
